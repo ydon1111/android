b700e76bee300b9af47a35ccef908035256d7dc2
kim-yeongdon <kyd5022@naver.com>
2022-10-30 17:14:53 +0900

    day2 sing_up

--- chattingApp/app/src/main/java/fastcampus/aop/part3/chattingapp/LoginActivity.kt
+++ chattingApp/app/src/main/java/fastcampus/aop/part3/chattingapp/LoginActivity.kt
@@ -1,10 +1,19 @@
+package fastcampus.aop.part3.chattingapp
+
 import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
+import com.google.firebase.auth.FirebaseAuth
+
+import fastcampus.aop.part3.chattingapp.databinding.ActivityLogInBinding
 
 class LogInActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityLogInBinding
+
+    lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +25,26 @@ class LogInActivity : AppCompatActivity() {
             val intent: Intent = Intent(this@LogInActivity, SignUpActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    /**
+     * 로그인
+     */
+    private fun login(email: String, password: String){
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    //성공 시 실행
+                    val intent: Intent = Intent(this@LogInActivity,
+                        MainActivity::class.java)
+                    startActivity(intent)
+                    Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+                    finish()
+                } else {
+                    //실패 시 실행
+                    Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+                    Log.d("Login", "Error: ${task.exception}")
+                }
+            }
     }
 }
