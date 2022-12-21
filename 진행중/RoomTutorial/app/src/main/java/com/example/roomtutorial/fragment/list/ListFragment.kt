9750db3b25197a1b1588d8e6bec2bad10baf5187
kim-yeongdon <kyd5022@naver.com>
2022-12-21 21:23:57 +0900

    readData and view

--- 진행중/RoomTutorial/app/src/main/java/com/example/roomtutorial/fragment/list/ListFragment.kt
+++ 진행중/RoomTutorial/app/src/main/java/com/example/roomtutorial/fragment/list/ListFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomtutorial.R
+import com.example.roomtutorial.data.UserViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 
 class ListFragment : Fragment() {
+
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,11 +25,22 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
 
+        // Recyclerview
+        val adapter = ListAdapter()
+        val recyclerView = view.recyclerview
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // UserViewModel
+        mUserViewModel = ViewModelProvider(this)[UserViewModel::class.java]
+        mUserViewModel.readAllData.observe(viewLifecycleOwner,Observer{ user ->
+            adapter.setData(user)
+        })
+
 
         view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
-
         return view
     }
 
