17ca496e0e4358e5c46b033eac861bac3754f83e
kim-yeongdon <kyd5022@naver.com>
2022-12-29 16:14:41 +0900

    저장공간 권한 요청 구현

--- 진행중/app/src/main/java/com/example/favoriteplacesapp/AddHappyPlaceActivity.kt
+++ 진행중/app/src/main/java/com/example/favoriteplacesapp/AddHappyPlaceActivity.kt
@@ -1,14 +1,33 @@
 package com.example.favoriteplacesapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.webkit.PermissionRequest
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.favoriteplacesapp.databinding.ActivityAddHappyPlaceBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityAddHappyPlaceBinding
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dataSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,5 +40,91 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         binding.toolbarAddPlace.setNavigationOnClickListener {
             onBackPressed()
         }
+
+//        binding.btnSave.se
+
+        dataSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+        binding.etDate.setOnClickListener(this)
+        binding.tvAddImage.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dataSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+            R.id.tv_add_image -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("옵션 선택")
+                val pictureDialogItems = arrayOf("갤러리에서 사진 선택", "사진 찍기")
+                pictureDialog.setItems(pictureDialogItems) { _, which ->
+                    when (which) {
+                        0 -> choosePhotoFromGallery()
+                        1
+                        -> Toast.makeText(this@AddHappyPlaceActivity, "카메라 선택", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+                pictureDialog.show()
+            }
+        }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withActivity(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    Toast.makeText(
+                        this@AddHappyPlaceActivity,
+                        "저장공간 읽기/쓰기 권한이 승인되었습니다.갤러리 이미지 사용가능합니다.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
+                token: PermissionToken?
+            ) {
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage("권한 승인이 필요합니다.")
+            .setPositiveButton("권한 설정 가기") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "yyyy.MM.dd"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        binding.etDate.setText(sdf.format(cal.time).toString())
     }
 }
