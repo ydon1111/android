4f96d64bed40d12f59573f9601393a1844f3dedd
kim-yeongdon <kyd5022@naver.com>
2023-01-10 10:57:06 +0900

    REST,JSON파싱완료

--- 진행중/ComposeStudy/marsPhotosApp/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ 진행중/ComposeStudy/marsPhotosApp/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,8 +1,10 @@
 package com.example.marsphotos.network
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+import okhttp3.MediaType.Companion.toMediaType
 
 
 private const val BASE_URL =
@@ -10,13 +12,15 @@ private const val BASE_URL =
 
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
     .baseUrl(BASE_URL)
     .build()
 
+
+
 interface MarsApiService {
     @GET("photos")
-    suspend fun getPhotos(): String
+    suspend fun getPhotos(): List<MarsPhoto>
 }
 
 object MarsApi {
