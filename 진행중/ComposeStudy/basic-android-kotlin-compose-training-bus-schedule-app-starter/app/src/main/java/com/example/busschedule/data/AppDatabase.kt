23dc74424e589ed5c60e1acaf9bd78b5f2cdd798
kim-yeongdon <kyd5022@naver.com>
2023-01-12 11:30:07 +0900

    Room연습 완료

---
+++ 진행중/ComposeStudy/basic-android-kotlin-compose-training-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/data/AppDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.busschedule.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = arrayOf(BusSchedule::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun busScheduleDao(): BusScheduleDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "app_database"
+                )
+                    .createFromAsset("database/bus_schedule.db")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also {
+                        INSTANCE = it
+                    }
+            }
+        }
+    }
+}
+
