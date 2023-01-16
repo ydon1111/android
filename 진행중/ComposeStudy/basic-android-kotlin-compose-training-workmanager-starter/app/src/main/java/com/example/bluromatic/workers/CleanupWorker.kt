e932d7b19931c18bb375eed8e92b1f8f1edbd06a
kim-yeongdon <kyd5022@naver.com>
2023-01-16 20:59:11 +0900

    WorkManger tutorial 아직 잘모르겠음

---
+++ 진행중/ComposeStudy/basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/CleanupWorker.kt
@@ -0,0 +1,52 @@
+package com.example.bluromatic.workers
+
+import android.content.Context
+import android.util.Log
+
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.OUTPUT_PATH
+import com.example.bluromatic.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import java.io.File
+
+
+private const val TAG = "CleanupWorker"
+
+class CleanupWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+
+    override suspend fun doWork(): Result {
+
+        makeStatusNotification(
+            applicationContext.resources.getString(R.string.cleaning_up_files),
+            applicationContext
+        )
+
+        return withContext(Dispatchers.IO) {
+            delay(DELAY_TIME_MILLIS)
+
+            return@withContext try {
+                val outputDirection = File(applicationContext.filesDir, OUTPUT_PATH)
+                if (outputDirection.exists()) {
+                    val entries = outputDirection.listFiles()
+                    if (entries != null) {
+                        for (entry in entries) {
+                            val name = entry.name
+                            if (name.isNotEmpty() && name.endsWith(".png")) {
+                                val deleted = entry.delete()
+                                Log.i(TAG, "Deleted $name - $deleted")
+                            }
+                        }
+                    }
+                }
+                Result.success()
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+                Result.failure()
+            }
+        }
+    }
+}
