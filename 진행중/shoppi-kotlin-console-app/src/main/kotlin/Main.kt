c0ca9975a2acddfce18f215ef195ccc7e487a3bf
kim-yeongdon <kyd5022@naver.com>
2023-01-26 13:06:37 +0900

    실습 1. 입력 받은 정보 출력

--- 진행중/shoppi-kotlin-console-app/src/main/kotlin/Main.kt
+++ 진행중/shoppi-kotlin-console-app/src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("안녕하세요, Shoppi에 오신 것을 환영합니다.")
+    println("쇼핑을 계속 하시려면 이름을 입력해주세요 :)")
+
+    val name = readLine()
+    println("""
+        감사합니다. 반갑습니다. $name 님
+        원하시는 카테고리를 입력해주세요. 
+        ***==================================***
+    """.trimIndent())
+
 }
