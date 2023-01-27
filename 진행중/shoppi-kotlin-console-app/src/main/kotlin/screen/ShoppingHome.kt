cbf14a5aa2a84def3fa9a70f30698f4eb697b9f4
kim-yeongdon <kyd5022@naver.com>
2023-01-27 11:41:09 +0900

    stack 을 사용한 화면 전환 준비

--- 진행중/shoppi-kotlin-console-app/src/main/kotlin/screen/ShoppingHome.kt
+++ 진행중/shoppi-kotlin-console-app/src/main/kotlin/screen/ShoppingHome.kt
@@ -3,7 +3,7 @@ package screen
 import LINE_DIVIDER
 import extensions.getNotEmptyString
 
-class ShoppingHome {
+class ShoppingHome : Screen()  {
 
     fun start() {
         showWelcomeMessage()
@@ -17,6 +17,7 @@ class ShoppingHome {
     }
 
     private fun showWelcomeMessage() {
+        ScreenStack.push(this)
         println("안녕하세요, Shoppi에 오신 것을 환영합니다.")
         println("쇼핑을 계속 하시려면 이름을 입력해주세요 :)")
 
